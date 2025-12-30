2345feb978bdbfa3590a8065ff370689df000d00
Maximiliano <techmax@adinet.com.uy>
2025-12-30 16:39:30 -0300

    error div

--- src/core/admin/components/SettingsForm.tsx
+++ src/core/admin/components/SettingsForm.tsx
@@ -514,7 +514,7 @@ const handleBackgroundDesktopDelete = async () => {
 
             
 
-              <div className="flex items-center gap-3 mb-6"></div>
+              <div className="flex items-center gap-3 mb-6">
                 <div className="p-2 bg-green-100 dark:bg-green-900/20 rounded-lg">
                   <ImageIcon className="w-5 h-5 text-green-600 dark:text-green-400" />
                 </div>
